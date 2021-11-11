8888002dae6118a2886483d42a2468d3f1fe6e3c
marios-stam <mariosstamatopoulos1@gmail.com>
2021-11-11 17:45:13 +0200

    wrote control scheme of VREP ,not tested

--- scripts/test.cpp
+++ scripts/test.cpp
@@ -1,37 +1,190 @@
 #include <ros/ros.h>
-#include <tf/transform_listener.h>
 #include <tf/LinearMath/Vector3.h>
+#include <tf/transform_listener.h>
 
-#include <geometry_msgs/Twist.h>
+#include <cmath>
+#include <eigen3/Eigen/Dense>
+
+Eigen::Matrix4f tfTransform_2_EigenMatrix(tf::StampedTransform transform)
+{
+    Eigen::Matrix4f mat;
+
+    // mat <<
+    // transform.getBasis().getRow(0).getX(),transform.getBasis().getRow(0).getY(),transform.getBasis().getRow(0).getZ(),transform.getOrigin().getX(),
+    // transform.getBasis().getRow(1).getX(),transform.getBasis().getRow(1).getY(),transform.getBasis().getRow(1).getZ(),transform.getOrigin().getY(),
+    // transform.getBasis().getRow(2).getX(),transform.getBasis().getRow(2).getY(),transform.getBasis().getRow(2).getZ(),transform.getOrigin().getZ(),
+    // 0,0,0,1;
+
+    tf::Vector3 pos = transform.getOrigin();
+    tf::Matrix3x3 rot = transform.getBasis();
+
+    mat << rot[0][0], rot[0][1], rot[0][2], pos[0], rot[1][0], rot[1][1],
+        rot[1][2], pos[1], rot[2][0], rot[2][1], rot[2][2], pos[2], 0, 0, 0, 1;
+    return mat;
+}
 
 
-int main(int argc, char** argv){
-    ros::init(argc, argv, "my_tf_listener");
-    //euler from quaternion
-    tf::Quaternion q;
+float vertical_control(double targetHeight , double height ,double vel_z ){
+	const float pParam = 2;
+    const float iParam = 0;
+    const float dParam = 0;
+    const float vParam = -2;
+
+    static float cumul = 0,lastE = 0;
+
+    double e = (targetHeight - height);
+    cumul = cumul + e;
+    float pv = pParam * e;
+
+    float thrust = 5.45 + pv + iParam * cumul + dParam * (e - lastE) + vel_z * vParam;
+    lastE = e;
+
+	return thrust;
+} 
+
+void horizontal_control(Eigen::Matrix4f mat,Eigen::Vector3f sp,float &alphaCorr,float &betaCorr){
+	Eigen::Vector4f vx, vy;
+    vx << 1, 0, 0, 0;
+    vy << 0, 1, 0, 0;
+
+    vx = mat * vx;
+    vy = mat * vy;
+
+    static double pAlphaE = 0,pBetaE = 0;
+
+    float alphaE = vy[2] - mat.coeff(2, 3);
+    alphaCorr = 0.25 * alphaE + 2.1 * (alphaE - pAlphaE);
+    pAlphaE = alphaE;
+
+    float betaE = vx[2] - mat.coeff(2, 3);
+    betaCorr = -0.25 * betaE - 2.1 * (betaE - pBetaE);
+    pBetaE = betaE;
+
+    static double cumulA = 0, cumulB = 0;
+
+    cumulA = cumulA + sp[1]; 
+	cumulB = cumulB + sp[0];
+
+	float threshold = 0.5;
+
+    static double psp1 = 0, psp2 = 0;
+    if (abs(psp2) > threshold && abs(sp[2]) < threshold)
+        cumulA = 0;
+
+    if (abs(psp1) > threshold && abs(sp[1]) < threshold) 
+		cumulB = 0;
+
+    float kp = 0.08;
+    float ki = 0.005;
+    float kd = 3;
+
+    alphaCorr = alphaCorr + kp * sp[1] + kd * (sp[1] - psp2) + ki *cumulA ;
+	betaCorr  = betaCorr  - kp * sp[0] - kd * (sp[0] - psp1) - ki *cumulB;
+
+	psp2 = sp[1];
+    psp1 = sp[0];
+}
+
+double rotational_control(float yaw , float des_yaw){
+	static float pYaw = 0;
     
+	double yaw_error = des_yaw - yaw;
+    double rotCorr = yaw_error * 0.1 + 2 * (yaw_error - pYaw);
+    pYaw = yaw;
+
+	return rotCorr;
+}
+
+void control(tf::StampedTransform transform, tf::StampedTransform vel_transform,
+             tf::StampedTransform des_transform)
+{
+    // get relative position
+    Eigen::Matrix4f mat = tfTransform_2_EigenMatrix(transform);
+
+    Eigen::Vector3f pos = mat.block<3, 1>(0, 3);
+    tf::Vector3 sp_tf = des_transform.getOrigin() - transform.getOrigin();
+    Eigen::Vector3f sp = Eigen::Vector3f(sp_tf.getX(), sp_tf.getY(), sp_tf.getZ());
+
+	tf::Vector3 l = vel_transform.getOrigin();
+
+    double targetHeight = des_transform.getOrigin().getZ();
+    double height = transform.getOrigin().getZ();
+	double vel_z  = l[2];
+
+	// Vertical control:
+    float thrust = vertical_control(targetHeight, height, vel_z);
+
+    // Horizontal control:
+    float alphaCorr,betaCorr;
+	horizontal_control(mat,sp,alphaCorr,betaCorr);
+
+    // Rotational control:
+    double des_euler[3],euler[3];
+	des_transform.getBasis().getEulerYPR(des_euler[0], des_euler[1], des_euler[2] );
+	transform.getBasis().getEulerYPR(euler[0], euler[1], euler[2] );
+	
+	double rotCorr = rotational_control(euler[0],des_euler[0]);
+    
+
+    double vel[4];
+    vel[0] = thrust * (1 - alphaCorr + betaCorr + rotCorr);
+	vel[1] = thrust * (1 - alphaCorr - betaCorr - rotCorr); 
+	vel[2] = thrust * (1 + alphaCorr - betaCorr + rotCorr); 
+	vel[3] = thrust * (1 + alphaCorr + betaCorr - rotCorr);
+
+	ROS_INFO("Thrust: %f  alphaCorr: %f  betaCorr: %f rotCorr: %f",thrust,alphaCorr,betaCorr,rotCorr);
+
+	// ROS_INFO("Rotor RPMs");
+	// ROS_INFO("%f %f %f %f",vel[0],vel[1],vel[2],vel[3]);
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "my_tf_listener");
+    // euler from quaternion
+    tf::Quaternion q;
+
     ros::NodeHandle node;
 
-  
     tf::TransformListener listener;
 
     ros::Rate rate(10.0);
-    while (node.ok()){
-        tf::StampedTransform transform;
-        try{
-            listener.lookupTransform("/world", "/drone_0",
-                               ros::Time(0), transform);
+    while (node.ok())
+    {
+        tf::StampedTransform transform_pos, transform_vel;
+        try
+        {
+            listener.lookupTransform("/world", "/drone_0_pos", ros::Time(0),
+                                     transform_pos);
+            listener.lookupTransform("/world", "/drone_0_vel", ros::Time(0),
+                                     transform_vel);
         }
-        catch (tf::TransformException &ex) {
-            ROS_ERROR("%s",ex.what());
+        catch (tf::TransformException &ex)
+        {
+            ROS_ERROR("%s", ex.what());
             ros::Duration(1.0).sleep();
             continue;
         }
 
-        tf::Vector3 pos = transform.getOrigin();
-        ROS_INFO("x: %f, y: %f, z: %f", pos.x(), pos.y(), pos.z());
+        tf::Vector3 pos = transform_pos.getOrigin();
+        // ROS_INFO("x: %f, y: %f, z: %f", pos.x(), pos.y(), pos.z());
+
+        tf::Matrix3x3 rot = transform_pos.getBasis();
+        double roll, pitch, yaw;
+        rot.getRPY(roll, pitch, yaw);
+        // ROS_INFO("roll: %f, pitch: %f, yaw: %f", roll, pitch, yaw);
+
+        pos = transform_vel.getOrigin();
+        // ROS_INFO("x_vel: %f, y_vel: %f, z_vel: %f", pos.x(), pos.y(), pos.z());
+
+        tf::StampedTransform des_transform;
+        des_transform.setOrigin(tf::Vector3(0, 0, 2));
+        des_transform.setRotation(tf::Quaternion(0, 0, 0, 1));
+        des_transform.stamp_ = ros::Time::now();
+
+        control(transform_pos, transform_vel, des_transform);
 
         rate.sleep();
     }
-  return 0;
+    return 0;
 };
---
+++ src/lua_controller.lua
@@ -0,0 +1,268 @@
+function sysCall_init() 
+    particlesAreVisible=true
+    simulateParticles=false
+    fakeShadow=true
+    
+    particleCountPerSecond=430
+    particleSize=0.005
+    particleDensity=8500
+    particleScatteringAngle=30
+    particleLifeTime=0.5
+    maxParticleCount=50
+    
+    if simROS then
+        --Rotors control
+        rotors_ctrl_topic = 'rotors_RPM'
+        rotors_ctrl_sub = simROS.subscribe('/'..rotors_ctrl_topic,'std_msgs/Float32MultiArray','rotors_ctrl_cb')
+        --Position control
+        position_ctrl_topic = 'desired_pos'
+        position_ctrl_sub = simROS.subscribe('/'..position_ctrl_topic,'std_msgs/Float32MultiArray','desired_pos_ctrl_cb')
+        
+        --Velocity publisher 
+        vel_pub=simROS.advertise('/drone_velocity', 'std_msgs/Float32MultiArray')
+        simROS.publisherTreatUInt8ArrayAsString(vel_pub) -- treat uint8 arrays as strings (much faster, tables/arrays are kind of slow in Lua)
+        
+        
+    end
+    
+
+    -- Detatch the manipulation sphere:
+    targetObj=sim.getObjectHandle('Quadcopter_target#0')
+    sim.setObjectParent(targetObj,-1,true)
+
+    -- This control algo was quickly written and is dirty and not optimal. It just serves as a SIMPLE example
+    d=sim.getObjectHandle('Quadcopter_base#0')
+
+    propellerHandles={}
+    jointHandles={}
+    particleObjects={-1,-1,-1,-1}
+    local ttype=sim.particle_roughspheres+sim.particle_cyclic+sim.particle_respondable1to4+sim.particle_respondable5to8+sim.particle_ignoresgravity
+    if not particlesAreVisible then
+        ttype=ttype+sim.particle_invisible
+    end
+    for i=1,4,1 do
+        propellerHandles[i]=sim.getObjectHandle('Quadcopter_propeller_respondable'..i)
+        jointHandles[i]=sim.getObjectHandle('Quadcopter_propeller_joint'..i)
+        if simulateParticles then
+            particleObjects[i]=sim.addParticleObject(ttype,particleSize,particleDensity,{2,1,0.2,3,0.4},particleLifeTime,maxParticleCount,{0.3,0.7,1})
+        end
+    end
+    heli=sim.getObjectHandle(sim.handle_self)
+
+    pParam=2
+    iParam=0
+    dParam=0
+    vParam=-2
+
+    cumul=0
+    lastE=0
+    pAlphaE=0
+    pBetaE=0
+    psp2=0
+    psp1=0
+
+    prevEuler=0
+cumulA=0
+    cumulB=0
+
+    if (fakeShadow) then
+        shadowCont=sim.addDrawingObject(sim.drawing_discpoints+sim.drawing_cyclic+sim.drawing_25percenttransparency+sim.drawing_50percenttransparency+sim.drawing_itemsizes,0.2,0,-1,1)
+    end
+end
+
+function sysCall_cleanup() 
+    sim.removeDrawingObject(shadowCont)
+    for i=1,#particleObjects,1 do
+        sim.removeParticleObject(particleObjects[i])
+    end
+end 
+
+
+CONTROL_FROM_ROS=false 
+vel={0,0,0,0}
+function sysCall_actuation() 
+    pos=sim.getObjectPosition(d,-1)
+    if (fakeShadow) then
+        itemData={pos[1],pos[2],0.002,0,0,1,0.2}
+        sim.addDrawingObjectItem(shadowCont,itemData)
+    end
+    quat = sim.getObjectQuaternion(d,-1)
+     if simROS then
+        --Position Transformation
+        pos_transform_msg={
+            header={
+                stamp=simROS.getTime(),
+                frame_id="/world"
+            },
+            child_frame_id="/drone_0_pos",
+            transform={
+                -- ROS has definition x=front y=side z=up
+                translation={x=pos[1],y=pos[2],z=pos[3]},--V-rep
+                rotation={x=quat[1],y=quat[2],z=quat[3],w=quat[4]}--v-rep
+            }
+        }
+        --print(pos_transform_msg)
+        simROS.sendTransform(pos_transform_msg)
+        
+        --Velocity Transformation
+        l=sim.getVelocity(heli)
+    
+        pos_transform_msg={
+            header={
+                stamp=simROS.getTime(),
+                frame_id="/world"
+            },
+            child_frame_id="/drone_0_vel",
+            transform={
+                -- ROS has definition x=front y=side z=up
+                translation={x=l[1],y=l[2],z=l[3]},--V-rep
+                rotation={x = 1 , y = 2 , z = 3 , w = 4 }--v-rep
+            }
+        }
+        --print(pos_transform_msg)
+        simROS.sendTransform(pos_transform_msg)
+        
+
+        -- velocities={}
+        -- velocities.data = { l[1],l[2],l[3] }
+        -- print(velocities)
+        -- simROS.publish(vel_pub,velocities)
+        
+    else
+        sim.addLog(sim.verbosity_scripterrors,"ROS interface was not found. Cannot run.")
+    end
+    
+    -- Vertical control:
+    targetPos=sim.getObjectPosition(targetObj,-1)
+    pos=sim.getObjectPosition(d,-1)
+    l=sim.getVelocity(heli)
+    e=(targetPos[3]-pos[3])
+    cumul=cumul+e
+    pv=pParam*e
+    thrust=5.45+pv+iParam*cumul+dParam*(e-lastE)+l[3]*vParam
+    lastE=e
+    
+   -- Horizontal control: 
+    sp=sim.getObjectPosition(targetObj,d)
+    --print(sp)
+    m=sim.getObjectMatrix(d,-1)
+    vx={1,0,0}
+    vx=sim.multiplyVector(m,vx)
+    vy={0,1,0}
+    vy=sim.multiplyVector(m,vy)
+    alphaE=(vy[3]-m[12])
+    
+    --print("cumulA",cumulA)
+    --alphaCorr=0.25*alphaE+2.1*(alphaE-pAlphaE)
+    alphaCorr=0.25*alphaE +2.1*(alphaE-pAlphaE) 
+    betaE=(vx[3]-m[12])
+    --print("errorA",alphaE)
+    --print("errorB",betaE)
+    --print("cumulB",cumulB)
+    --betaCorr=-0.25*betaE-2.1*(betaE-pBetaE)
+    betaCorr=-0.25*betaE -2.1*(betaE-pBetaE)
+    pAlphaE=alphaE
+    pBetaE=betaE
+    cumulA=cumulA+sp[2]
+    cumulB=cumulB+sp[1]
+    threshold=0.5
+    if (math.abs(psp2)>threshold and math.abs(sp[2])<threshold) then
+        cumulA=0
+    end
+    if (math.abs(psp1)>threshold and math.abs(sp[1])<threshold) then
+        cumulB=0
+    end
+    
+    --print('eB',sp[1])
+    kp=0.08
+    ki=0.005
+    kd=3
+    --alphaCorr=alphaCorr+sp[2]*0.005+1*(sp[2]-psp2)
+    --betaCorr=betaCorr-sp[1]*0.005-1*(sp[1]-psp1)
+    alphaCorr = alphaCorr + kp*sp[2] + kd*(sp[2]-psp2) + ki*cumulA
+    betaCorr  = betaCorr  - kp*sp[1] - kd*(sp[1]-psp1) - ki*cumulB
+    
+    psp2=sp[2]
+    psp1=sp[1]
+
+    
+    -- Rotational control:
+    euler=sim.getObjectOrientation(d,targetObj)
+    rotCorr=euler[3]*0.1+2*(euler[3]-prevEuler)
+    prevEuler=euler[3]
+    
+    vel[0] = thrust*(1-alphaCorr+betaCorr+rotCorr)
+    vel[1] = thrust*(1-alphaCorr-betaCorr-rotCorr)
+    vel[2] = thrust*(1+alphaCorr-betaCorr+rotCorr)
+    vel[3] = thrust*(1+alphaCorr+betaCorr-rotCorr)
+    -- Decide of the motor velocities:
+    handlePropeller(1 , vel[0] )
+    handlePropeller(2 , vel[1] )
+    handlePropeller(3 , vel[2] )
+    handlePropeller(4 , vel[3] )
+end 
+
+function rotors_ctrl_cb(msg)
+    -- rotors control subscriber callback
+    CONTROL_FROM_ROS=true
+    
+end
+
+function desired_pos_ctrl_cb(msg)
+    -- Right motor speed subscriber callback
+    sim.setJointTargetVelocity(rightMotor,msg.data)
+end
+
+
+function handlePropeller(index,particleVelocity)
+    propellerRespondable=propellerHandles[index]
+    propellerJoint=jointHandles[index]
+    propeller=sim.getObjectParent(propellerRespondable)
+    particleObject=particleObjects[index]
+    maxParticleDeviation=math.tan(particleScatteringAngle*0.5*math.pi/180)*particleVelocity
+    notFullParticles=0
+
+    local t=sim.getSimulationTime()
+    sim.setJointPosition(propellerJoint,t*10)
+    ts=sim.getSimulationTimeStep()
+    
+    m=sim.getObjectMatrix(propeller,-1)
+    particleCnt=0
+    pos={0,0,0}
+    dir={0,0,1}
+    
+    requiredParticleCnt=particleCountPerSecond*ts+notFullParticles
+    notFullParticles=requiredParticleCnt % 1
+    requiredParticleCnt=math.floor(requiredParticleCnt)
+    while (particleCnt<requiredParticleCnt) do
+        -- we want a uniform distribution:
+        x=(math.random()-0.5)*2
+        y=(math.random()-0.5)*2
+        if (x*x+y*y<=1) then
+            if (simulateParticles) then
+                pos[1]=x*0.08
+                pos[2]=y*0.08
+                pos[3]=-particleSize*0.6
+                dir[1]=pos[1]+(math.random()-0.5)*maxParticleDeviation*2
+                dir[2]=pos[2]+(math.random()-0.5)*maxParticleDeviation*2
+                dir[3]=pos[3]-particleVelocity*(1+0.2*(math.random()-0.5))
+                pos=sim.multiplyVector(m,pos)
+                dir=sim.multiplyVector(m,dir)
+                itemData={pos[1],pos[2],pos[3],dir[1],dir[2],dir[3]}
+                sim.addParticleObjectItem(particleObject,itemData)
+            end
+            particleCnt=particleCnt+1
+        end
+    end
+    -- Apply a reactive force onto the body:
+    totalExertedForce=particleCnt*particleDensity*particleVelocity*math.pi*particleSize*particleSize*particleSize/(6*ts)
+    force={0,0,totalExertedForce}
+    m[4]=0
+    m[8]=0
+    m[12]=0
+    force=sim.multiplyVector(m,force)
+    local rotDir=1-math.mod(index,2)*2
+    torque={0,0,rotDir*0.002*particleVelocity}
+    torque=sim.multiplyVector(m,torque)
+    sim.addForceAndTorque(propellerRespondable,force,torque)
+end
